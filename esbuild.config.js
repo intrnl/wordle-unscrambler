2d5ca39b9a3d576de47b3d41d1fd01be96a7e6e2
intrnl <intrnl28+git@gmail.com>
2022-01-14 09:31:50 +0700

    initial commit

---
+++ esbuild.config.js
@@ -0,0 +1,18 @@
+import * as esbuild from 'esbuild';
+
+import velvet from '@intrnl/esbuild-plugin-velvet';
+
+
+/** @type {esbuild.BuildOptions} */
+export let config = {
+	entryPoints: ['src/App.velvet'],
+	entryNames: 'app',
+	outdir: 'dist/_assets',
+	publicPath: '/_assets/',
+
+	sourcemap: true,
+
+	plugins: [
+		velvet(),
+	],
+};
